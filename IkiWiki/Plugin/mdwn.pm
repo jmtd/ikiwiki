5b1f0261d1576c6cf2d55c2941d11d290b9b9041
Jonathan Dowland <jon@dow.land>
2021-11-08 13:46:28 +0000

    mdwn: debug() which implementation is chosen

--- IkiWiki/Plugin/mdwn.pm
+++ IkiWiki/Plugin/mdwn.pm
@@ -74,6 +74,7 @@ sub htmlize (@) {
 				debug(gettext("multimarkdown is enabled, but Text::MultiMarkdown is not installed"));
 			}
 			else {
+				debug(gettext("mdwn: using multimarkdown"));
 				$markdown_sub=sub {
 					my %flags=( use_metadata => 0 );
 
@@ -136,6 +137,7 @@ sub htmlize (@) {
 					$always_flags |= 0x08000000;
 				}
 
+				debug(gettext("mdwn: using discount"));
 				$markdown_sub=sub {
 					my $t=shift;
 
@@ -170,15 +172,18 @@ sub htmlize (@) {
 			eval q{use Text::Markdown};
 			if (! $@) {
 				if (Text::Markdown->can('markdown')) {
+					debug(gettext("mdwn: using Text::Markdown::markdown"));
 					$markdown_sub=\&Text::Markdown::markdown;
 				}
 				else {
+					debug(gettext("mdwn: using Text::Markdown::Markdown"));
 					$markdown_sub=\&Text::Markdown::Markdown;
 				}
 			}
 			else {
 				eval q{use Markdown};
 				if (! $@) {
+					debug(gettext("mdwn: using Markdown::Markdown"));
 					$markdown_sub=\&Markdown::Markdown;
 				}
 				else {
