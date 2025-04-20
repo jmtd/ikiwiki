0e771f67020ecb58e0f5359825a8ae2a3c93dcd7
Jonathan Dowland <jon@dow.land>
2025-04-20 21:25:05 +0100

    po4a: support versions pre 0.70 for interim period
    Joey pointed out that the changes to support 0.70+ broke building
    IkiWiki on Debian stable (bullseye) which has po4a 0.69. Fix this
    by checking for several methods that were exported prior to 0.70
    and removed from 0.70 onwards.
    
    Sadly po4a does not export a module $VERSION so check for the
    relevant methods (detected_charset, set_charset) instead.

--- IkiWiki/Plugin/po.pm
+++ IkiWiki/Plugin/po.pm
@@ -906,11 +906,21 @@ sub refreshpot ($) {
 					   po4a_options($masterfile));
 	$doc->read($masterfile,"?");
 	$doc->{TT}{utf_mode} = 1;
+
+	# po4a ≥ 0.70 removed several methods
+	if($doc->can("detected_charset")) {
+		$doc->detected_charset('UTF-8');
+		$doc->{TT}{po_in}->set_charset('UTF-8');
+	}
+
 	# let's cheat a bit to force porefs option to be passed to
 	# Locale::Po4a::Po; this is undocument use of internal
 	# Locale::Po4a::TransTractor's data, compulsory since this module
 	# prevents us from using the porefs option.
 	$doc->{TT}{po_out}=Locale::Po4a::Po->new({ 'porefs' => 'none' });
+	if($doc->can("set_charset")) {
+		$doc->{TT}{po_out}->set_charset('UTF-8');
+	}
 	# do the actual work
 	$doc->parse;
 	IkiWiki::prep_writefile(basename($potfile),dirname($potfile));
